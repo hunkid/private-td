64c2cb4f5f7a8cd8e9e49ad50f93214c5200d37f
hunkid <273946286@qq.com>
2017-11-01 13:16:40 +0800

    更新配置

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -149,7 +149,13 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+              plugins: [
+                ['import', [{ libraryName: "antd", style: 'css' }]],
+              ],
+              // This is a feature of `babel-loader` for webpack (not Babel itself).
+              // It enables caching results in ./node_modules/.cache/babel-loader/
+              // directory for faster rebuilds.
+              cacheDirectory: true,
               compact: true,
             },
           },
